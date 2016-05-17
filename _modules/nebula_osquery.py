0f568972161b3a6cb017c9a84a6bcfdadc493ca1
Colton Myers <colton.myers@gmail.com>
2016-05-17 12:52:15 -0600

    Add configurable pillar location

--- _modules/nebula_osquery.py
+++ _modules/nebula_osquery.py
@@ -39,7 +39,7 @@ def __virtual__():
     return __virtualname__
 
 
-def queries(query_group, verbose=False):
+def queries(query_group, verbose=False, pillar_key='nebula_osquery'):
     '''
     Run the set of queries represented by ``query_group`` from the
     configuration in the pillar key ``nebular_osquery``.
@@ -50,8 +50,9 @@ def queries(query_group, verbose=False):
 
         salt '*' nebula.queries day
         salt '*' nebula.queries hour verbose=True
+        salt '*' nebula.queries hour pillar_key=sec_osqueries
     '''
-    query_data = __salt__['pillar.get']('nebula_osquery:{0}'.format(query_group), [])
+    query_data = __salt__['pillar.get']('{0}:{1}'.format(pillar_key, query_group), [])
 
     if not query_data:
         return None
